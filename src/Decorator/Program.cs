0dc9c1f3bf79aff9ac8daddbaebeb36bb042b74f
wodson <wodsonluiz@live.com>
2025-01-25 19:34:28 -0300

    feat: Add pattern Decorator :dizzy:

---
+++ src/Decorator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Decorator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // instantiate mail service
+        var cloudMailService = new CloudMailService();
+        cloudMailService.SendMail("Hi there.");
+
+        var onPremiseMailService = new OnPremiseMailService();
+        onPremiseMailService.SendMail("Hi there");
+
+        // add behavior
+        var statisticsDecorator = new StatisticsDecorator(cloudMailService);
+        statisticsDecorator.SendMail($"He there via {nameof(StatisticsDecorator)} wrapper");
+
+        var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+        messageDatabaseDecorator.SendMail( $"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message1.");
+        messageDatabaseDecorator.SendMail( $"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message2.");
+
+        foreach (var message in messageDatabaseDecorator.SentMessages)
+        {
+            Console.WriteLine($"Stored message: \"{message}\"");
+        }
+    }
+}
