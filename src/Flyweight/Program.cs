536ba15f3b414a19784ff5a1523f48814b1fe794
Wodson Correia <wodsonluiz@live.com>
2025-05-01 17:20:20 -0300

    Adiciona padrao Flyweight (#11)
    * feat: Add project
    
    * feat: Add implementation

---
+++ src/Flyweight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Flyweight;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var aBunchOfCharacters = "abba";
+        var characterFactory = new CharecterFactory();
+
+        var characterObject = characterFactory.GetCharacter(aBunchOfCharacters[0]);
+        characterObject.Draw("Maria", 13);
+
+        characterObject = characterFactory.GetCharacter(aBunchOfCharacters[1]);
+        characterObject.Draw("Joao", 16);
+
+        characterObject = characterFactory.GetCharacter(aBunchOfCharacters[2]);
+        characterObject.Draw("Simone", 34);
+
+        characterObject = characterFactory.GetCharacter(aBunchOfCharacters[3]);
+        characterObject.Draw("Wodson", 34);
+
+        var paragraph = characterFactory.CreateParagraph(new List<ICharacter>() { characterObject }, 1);
+
+        paragraph.Draw("Antonio", 25);
+    }
+}
