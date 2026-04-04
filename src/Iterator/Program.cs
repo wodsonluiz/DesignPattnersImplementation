9f7ca30bf6fdb27afb190bf82a58159eb58bfb08
wodsonluiz <wodsonluiz@live.com>
2026-04-04 18:33:26 -0300

    feat: Add more projects

---
+++ src/Iterator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Iterator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create collection
+        PeopleCollection people = new();
+
+        people.Add(new Person("Wodson Correia", "Brazil"));
+        people.Add(new Person("Joao Correia", "Brazil"));
+        people.Add(new Person("Maria Correia", "Brazil"));
+        people.Add(new Person("Nubia Correia", "Brazil"));
+
+        //create iterator
+        var peopleIterator = people.CreateIterator();
+
+        for(Person person = peopleIterator.First(); !peopleIterator.IsDone; person = peopleIterator.Next())
+        {
+            Console.WriteLine(person?.Name);
+        }
+    }
+}
---
+++ src/UnitOfWork/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UnitOfWork;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
